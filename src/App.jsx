94f987b6c4df5ef3f1c3be7d33e1e66552484742
MarcioAlex-x <marcioalex586@gmail.com>
2024-07-06 11:54:40 -0300

    PRimeiro commit da aula

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import Botao from "./Botao"
+import Header from "./Header"
+
+
+const App = () =>{
+  const nome = 'turma'
+  const diaSemana = 'sábado'
+  const n1 = 12
+  const n2 = 6
+  return(
+    <> {/* Fragment */}
+      <Header />      
+      <Botao />
+    </>
+      
+    
+  )
+}
+
+export default App
